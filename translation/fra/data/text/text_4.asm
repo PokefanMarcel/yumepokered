6ea16fe4a5900f2d5732bf69d055a0415855d0da
MohnDoe <kfnpfeffer@gmail.com>
2025-08-22 21:19:03 +0200

    fix text 4

--- translation/fra/data/text/text_4.asm
+++ translation/fra/data/text/text_4.asm
@@ -230,3 +230,45 @@ ENDC
 	line "ami et revenez"
 	cont "plus tard!"
 	done
+
+
+_PokeBeeperAlertFullText:: ; TODO marcelnote - new for PokéBeeper
+	text "A message on the"
+	line "# BEEPER."
+
+	para "The BOX is full!"
+	line "Better find a"
+	cont "PC fast!"
+	done
+
+_RepelUseAnotherText:: ; TODO marcelnote - added prompt to reuse repel
+	text "Use another?"
+	done
+
+_PokeBeeperAlertText:: ; TODO marcelnote - new for PokéBeeper
+	text "A message on the"
+	line "# BEEPER!"
+
+	para "The BOX has space"
+	line "for just @"
+	text_decimal wStringBuffer, 1, 2
+	text " more"
+	cont "#MON."
+
+	para "Change it soon at"
+	line "a #MON CENTER!"
+	done
+
+_CanUseMoveText:: ; TODO marcelnote - for temporary field moves
+	text_ram wLearnMoveMonName
+	text " can use"
+	line "@"
+	text_ram wStringBuffer
+	text "!@"
+	text_end
+
+_PokemonCenterPerfectHealthText:: ; TODO marcelnote - broke down WelcomeText to skip this
+	text "We heal your"
+	line "#MON back to"
+	cont "perfect health!"
+	prompt
