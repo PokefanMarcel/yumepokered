d1c63ac31262b777357a2ec023052ab0282091dc
MohnDoe <kfnpfeffer@gmail.com>
2025-08-22 15:42:31 +0200

    fix: fix vermilioncity texts

--- translation/fra/text/VermilionCity.asm
+++ translation/fra/text/VermilionCity.asm
@@ -52,6 +52,31 @@ _VermilionCitySailor1YouNeedATicketText::
 	cont "à bord."
 	done
 
+_VermilionCitySailor3WelcomeToFerriesText:: ;TODO marcelnote - new for ferry
+	text "Welcome to"
+	line "CITRUS FERRIES!"
+	done
+
+_VermilionCitySailor3ShowedPassText:: ;TODO marcelnote - new for ferry
+	text "Welcome to"
+	line "CITRUS FERRIES!"
+
+	para "<PLAYER> showed"
+	line "the CITRUS PASS."
+
+	para "Perfect! Right"
+	line "this way, please."
+	done
+
+_VermilionCitySailor3YouNeedPassText:: ;TODO marcelnote - new for ferry
+	text "Welcome to"
+	line "CITRUS FERRIES!"
+
+	para "I'm sorry, you"
+	line "need an CITRUS"
+	cont "PASS to board."
+	done
+
 _VermilionCitySailor1ShipSetSailText::
 	text "L'OCEANE a"
 	line "levé l'ancre."
@@ -129,4 +154,39 @@ _VermilionCityGymSignText::
 
 _VermilionCityHarborSignText::
 	text "PORT DE CARMIN"
+	done
+
+
+_VermilionCityPhoneText:: ; TODO marcelnote - new
+	text "A pay phone?"
+
+	para "It doesn't seem"
+	line "to be working..."
+	done
+
+_VermilionCityCooltrainerFText:: ; TODO
+	text "The S.S.ANNE party"
+	line "was amazing!"
+
+	para "I'm heading next to"
+	line "MANDARIN ISLAND."
+	cont "The ferry runs"
+	cont "there regularly."
+	done
+
+_VermilionCityCooltrainerMText:: ; TODO marcelnote - to explain temporary field moves
+;	text "If you won't use"
+;	line "an HM in battle,"
+;	cont "teach a #MON"
+;	cont "to use it only"
+;	cont "outside battles!"
+	text "If you don't need"
+	line "an HM in battle,"
+	cont "just teach it as"
+	cont "a SKILL to use"
+	cont "outside battles!"
+
+	para "It also works for"
+	line "moves like DIG or"
+	cont "TELEPORT."
 	done
