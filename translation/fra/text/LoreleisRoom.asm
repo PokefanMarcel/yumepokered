e08cd87d9a8c3d7700f203ae2854de9ab43d536e
MohnDoe <kfnpfeffer@gmail.com>
2025-08-22 15:51:50 +0200

    fix: loreleisroom

--- translation/fra/text/LoreleisRoom.asm
+++ translation/fra/text/LoreleisRoom.asm
@@ -35,6 +35,45 @@ _LoreleisRoomLoreleiAfterBattleText::
 	cont "zurprizes!"
 	done
 
+
+_LoreleisRoomLoreleiRematchBeforeBattleText:: ; TODO marcelnote - Lorelei rematch text
+	text "Welcome back to"
+	line "#MON LEAGUE,"
+	cont "champion!"
+
+	para "After our stay in"
+	line "the sun, my"
+	cont "#MON are eager"
+	cont "for a fresh new"
+	cont "challenge!"
+
+	para "Bundle up, it's"
+	line "about to get"
+	cont "freezing cold!"
+	done
+
+_LoreleisRoomLoreleiRematchEndBattleText:: ; TODO marcelnote - Lorelei rematch text
+	text "You've"
+	line "melted us worse"
+	cont "than a beach day!"
+	prompt
+
+_LoreleisRoomLoreleiRematchAfterBattleText:: ; TODO marcelnote - Lorelei rematch text
+	text "Your freedom is"
+	line "inspiring,"
+	cont "<PLAYER>..."
+
+	para "Now that my home"
+	line "needs help, I"
+	cont "may soon follow"
+	cont "your example." ; TODO maybe not explicit enough?
+
+	para "But while I'm"
+	line "still here, I'll"
+	cont "give it my all!"
+	done
+
+
 _LoreleisRoomLoreleiDontRunAwayText::
 	text "Une voix:"
 	line "Ne fuis pas!"
