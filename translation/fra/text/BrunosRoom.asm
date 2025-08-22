9ee9876575219d7a6b67f43080a93c60f9b8e2a5
MohnDoe <kfnpfeffer@gmail.com>
2025-08-22 15:53:18 +0200

    fix: brunosroom

--- translation/fra/text/BrunosRoom.asm
+++ translation/fra/text/BrunosRoom.asm
@@ -1,3 +1,70 @@
+_BrunosRoomBrunoBeforeBattleText:: ; TODO marcelnote - added map name to label
+	text "I am BRUNO of"
+	line "the ELITE FOUR!"
+
+	para "Through rigorous"
+	line "training, people"
+	cont "and #MON can"
+	cont "become stronger!"
+
+	para "I've weight"
+	line "trained with"
+	cont "my #MON!"
+
+	para "<PLAYER>!"
+
+	para "We will grind you"
+	line "down with our"
+	cont "superior power!"
+
+	para "Hoo hah!"
+	done
+
+_BrunosRoomBrunoEndBattleText:: ; TODO marcelnote - added map name to label
+	text "Why?"
+	line "How could I lose?"
+	prompt
+
+_BrunosRoomBrunoAfterBattleText:: ; TODO marcelnote - added map name to label
+	text "My job is done!"
+	line "Go face your next"
+	cont "challenge!"
+	done
+
+_BrunosRoomBrunoRematchBeforeBattleText:: ; TODO marcelnote - Bruno rematch text
+	text "Ready for another"
+	line "round, champ?"
+
+	para "My #MON and I"
+	line "have kept to a"
+	cont "strict workout"
+	cont "and diet routine."
+
+	para "Face the results"
+	line "of our rigorous"
+	cont "training!"
+
+	para "Hoo hah!"
+	done
+
+_BrunosRoomBrunoRematchEndBattleText:: ; TODO marcelnote - Bruno rematch text
+	text "So my"
+	line "training is"
+	cont "still lacking..."
+	prompt
+
+_BrunosRoomBrunoRematchAfterBattleText:: ; TODO marcelnote - Bruno rematch text
+	text "True discipline is"
+	line "not accomplished"
+	cont "in a day."
+
+	para "Constant training"
+	line "over time is key."
+
+	para "On to the next"
+	line "battle, champ!"
+	done
+
 _BrunoBeforeBattleText::
 	text "Mon nom est ALDO"
 	line "du CONSEIL des 4!"
