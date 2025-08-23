c4c741a3b9927113faa7ba774450b666a64a691a
Pokefan Marcel <166040321+PokefanMarcel@users.noreply.github.com>
2025-08-23 21:49:08 +0200

    Update naming_screen.asm

--- engine/menus/naming_screen.asm
+++ engine/menus/naming_screen.asm
@@ -458,9 +458,9 @@ PrintNamingText:
 	call GetMonName
 	hlcoord 4, 1
 	call PlaceString
-	ld hl, $1
-	add hl, bc
-	ld [hl], "の" ; leftover from Japanese version; blank tile $c9 in English
+;	ld hl, $1 ; marcelnote - removed
+;	add hl, bc
+;	ld [hl], "の" ; leftover from Japanese version; blank tile $c9 in English
 	hlcoord 1, 3
 	ld de, NicknameTextString
 	jr .placeString
