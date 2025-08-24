36ee713e44affb43eaefe586088691a3f742b7a6
Pokefan Marcel <166040321+PokefanMarcel@users.noreply.github.com>
2025-08-24 15:16:28 +0200

    Translation FRA

---
+++ data/text/options.asm
@@ -0,0 +1,40 @@
+; marcelnote - moved from engine/menus/options.asm
+AllOptionsText:
+	db   "TEXT SPEED"
+	next "BATTLE ANIMATION"
+	next "BATTLE STYLE"
+	next "MEASURE UNITS"
+	next ""
+	next ""
+	next ""
+	next "DONE@" ; marcelnote - changed from CANCEL@
+
+OptionsColonsText:
+	db   ":"
+	next ":"
+	next ":"
+	next ":@"
+
+InstantText:
+	db "INSTANT@"
+FastText:
+	db "FAST   @"
+MediumText:
+	db "MEDIUM @"
+SlowText:
+	db "SLOW   @"
+
+AnimationOnText:
+	db "ON @"
+AnimationOffText:
+	db "OFF@"
+
+BattleStyleShiftText:
+	db "SHIFT@"
+BattleStyleSetText:
+	db "SET  @"
+
+MeasureUnitsImperialText:
+	db "IMPERIAL@"
+MeasureUnitsMetricText:
+	db "METRIC  @"
