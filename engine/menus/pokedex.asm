e0caff6689e3254c731cee86d6d9411143dd3190
PokefanMarcel <166040321+PokefanMarcel@users.noreply.github.com>
2025-08-26 10:35:23 +0200

    Update pokedex.asm

--- engine/menus/pokedex.asm
+++ engine/menus/pokedex.asm
@@ -1033,16 +1033,12 @@ ENDC
 	ret
 
 
-
 IF DEF(_FRA)
-INCLUDE "translation/fra/data/pokemon/dex_entries.fra.asm"
-ELSE
-INCLUDE "data/pokemon/dex_entries.asm"
-ENDC
-INCLUDE "data/pokemon/dex_order.asm"
-
-IF DEF(_FRA)
+	INCLUDE "translation/fra/data/pokemon/dex_entries.fra.asm"
 	INCLUDE "translation/fra/data/text/pokedex.fra.asm"
 ELSE
+	INCLUDE "data/pokemon/dex_entries.asm"
 	INCLUDE "data/text/pokedex.asm"
 ENDC
+
+INCLUDE "data/pokemon/dex_order.asm"
