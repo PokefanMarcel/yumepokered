52f146a1932812869318b90c7a80b332b61d6f01
Pokefan Marcel <166040321+PokefanMarcel@users.noreply.github.com>
2025-10-26 01:16:53 +0200

    Daycare FRA fix files

--- translation/fra/text/Daycare.fra.asm
+++ translation/fra/text/Daycare.fra.asm
@@ -62,31 +62,39 @@ _DaycareGentlemanMonNeedsMoreTimeText::
 	cont "encore un peu."
 	prompt
 
-_DaycareGentlemanNotEnoughMoneyText:: ; TODO
-	text "Hey, you don't"
-	line "have enough ¥!"
-	done
-
-_DaycareGentlemanHeresYourMonText:: ; TODO
-	text "Thank you! Here's"
-	line "your #MON!"
-	prompt
-
-_DaycareGentlemanOnlyHaveOneMonText:: ; TODO
-	text "You only have one"
-	line "#MON with you."
-	done
-
-_DaycareGentlemanNoRoomForMonText:: ; TODO
-	text "You have no room"
-	line "for this #MON!"
-	done
-
-_DaycareGentlemanComeAgainText:: ; TODO
-	text "Come again."  ; marcelnote - capitalized 'come'
-	done
-
-_DaycareGentlemanAllRightThenText:: ; TODO
-	text "All right then,"
+_DaycareGentlemanAllRightThenText::
+	text "Parfait!"
 	line "@"
 	text_end
+
+_DaycareGentlemanComeAgainText::
+	text "A plus tard."
+	done
+
+_DaycareGentlemanNoRoomForMonText::
+	text "Tu n'as plus de"
+	line "place pour ce"
+	cont "#MON!"
+	done
+
+_DaycareGentlemanOnlyHaveOneMonText::
+	text "Tu n'as qu'un"
+	line "#MON sur toi."
+	done
+
+;_DaycareGentlemanCantAcceptMonWithHMText:: ; marcelnote - daycare can erase HM moves
+;	text "Je ne peux pas"
+;	line "m'occuper d'un"
+;	cont "#MON qui a"
+;	cont "appris une CS."
+;	done
+
+_DaycareGentlemanHeresYourMonText::
+	text "Merci! Voilà ton"
+	line "p'tit #MON!"
+	prompt
+
+_DaycareGentlemanNotEnoughMoneyText::
+	text "Hé! Tu n'as pas"
+	line "assez d'argent!"
+	done
