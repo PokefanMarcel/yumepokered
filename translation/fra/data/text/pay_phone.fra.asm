a04d722377100859a94e6de8fcedd996fa85f8de
Pokefan Marcel <166040321+PokefanMarcel@users.noreply.github.com>
2025-11-01 14:34:12 +0100

    Translation FRA pay phone

--- translation/fra/data/text/pay_phone.fra.asm
+++ translation/fra/data/text/pay_phone.fra.asm
@@ -1,7 +1,529 @@
 ; marcelnote - new for pay phones
 
-; TODO - translate
+_PayPhoneIntroText::
+	text "Un téléphone!"
+	line "C'est 10¥ l'appel."
+
+	para "Qui veux-tu"
+	line "appeler?"
+	done
+
+_PayPhoneMomIntroText::
+	text "..."
+
+	para "MAMAN: Bonjour?"
+
+	para "<PLAYER>, c'est"
+	line "toi! Où es-tu?"
+	prompt
+
+_PayPhoneMomOutroText::
+	text "Fais de ton mieux!"
+
+	para "..."
+	done
+
+_PayPhoneMomViridianText::
+	text "Ah, JADIELLE! Voir"
+	line "les rayons de"
+	cont "soleil à travers"
+	cont "les arbres..."
+	cont "C'est comme si le"
+	cont "temps s'arrêtait."
+
+	para "Fais attention"
+	line "dans la forêt,"
+	cont "d'accord?"
+	prompt
+
+_PayPhoneMomPewterText::
+	text "ARGENTA? On dit"
+	line "qu'après la pluie,"
+	cont "l'air y prend une"
+	cont "odeur minérale..."
+	cont "J’aimerais voir ça"
+	cont "un jour."
+
+	para "Je sais que tu te"
+	line "débrouilles bien,"
+	cont "mais appelle-moi"
+	cont "quand tu peux,"
+	cont "d’accord?"
+	prompt
+
+_PayPhoneMomCeruleanText::
+	text "Ah, AZURIA..."
+	line "Les fleurs qui"
+	cont "éclosent au bord"
+	cont "de l'eau... On"
+	cont "dirait un tableau"
+	cont "qui prend vie,"
+	cont "non?"
+
+	para "En s'arrêtant un"
+	line "peu, on voit le"
+	cont "monde autrement."
+	prompt
+
+_PayPhoneMomVermilionText::
+	text "CARMIN-SUR-MER!"
+	line "Avec ses grands"
+	cont "navires et leurs"
+	cont "voyageurs..."
+	cont "Imagine tous ces"
+	cont "gens que tu vas"
+	cont "rencontrer!"
+
+	para "N'oublie pas de"
+	line "bien te couvrir!"
+	cont "Le vent de la mer"
+	cont "peut être frais."
+	prompt
+
+_PayPhoneMomLavenderText::
+	text "LAVANVILLE? On dit"
+	line "souvent que c'est"
+	cont "une ville triste,"
+	cont "mais je la trouve"
+	cont "paisible, à sa"
+	cont "façon."
+
+	para "Le silence en dit"
+	line "plus que les"
+	cont "mots, parfois."
+	prompt
+
+_PayPhoneMomCeladonText::
+	text "Oh, CELADOPOLE..."
+	line "Le soir venu, la"
+	cont "ville s'illumine"
+	cont "avec ses néons"
+	cont "et ses enseignes!"
+
+	para "Essaie de ne pas"
+	line "dépenser tout ton"
+	cont "argent au centre"
+	cont "commercial!"
+	prompt
+
+_PayPhoneMomSaffronText::
+	text "Ah, SAFRANIA! Les"
+	line "grandes villes, ça"
+	cont "peut donner le"
+	cont "tournis, hein?"
+	cont "Là-bas, tout le"
+	cont "monde est pressé."
+
+	para "N'oublie pas que"
+	line "tu as le droit de"
+	cont "souffler un peu."
+	prompt
+
+_PayPhoneMomFuchsiaText::
+	text "La vue depuis"
+	line "les collines de"
+	cont "PARMANIE est"
+	cont "superbe, non?"
+	cont "Tous ces oiseaux"
+	cont "qui volent..."
+
+	para "Tu les regarderas"
+	line "un peu pour moi,"
+	cont "s'il-te-plaît?"
+	prompt
+
+_PayPhoneMomCinnabarText::
+	text "CRAMOIS'ILE? Quand"
+	line "la mer est calme,"
+	cont "on peut voir le"
+	cont "reflet du volcan"
+	cont "dans l'eau..."
+	cont "Ca doit être"
+	cont "très beau."
+
+	para "Tu as vraiment"
+	line "grandi depuis"
+	cont "ton départ,"
+	cont "<PLAYER>. Je suis"
+	cont "fière de toi."
+	prompt
+
+_PayPhoneMomMandarinText::
+	text "L'ILE MANDARINE!"
+	line "Tu es si loin de"
+	cont "la maison main-"
+	cont "tenant..."
+	cont "La mer est-elle"
+	cont "aussi bleue que"
+	cont "sur les photos?"
+
+	para "Peu importe où"
+	line "tu vas, tu auras"
+	cont "toujours une mai-"
+	cont "son qui t'attend."
+	prompt
+
+
+_PayPhoneDaisyOldRodText::
+	text "..."
+
+	para "NINA: Oui?"
+	line "Salut <PLAYER>!"
+
+	para "Tu es à JADIELLE?"
+	line "Passe donc voir"
+	cont "le MAITRE PECHEUR"
+	cont "sur la ROUTE 22."
+
+	para "Il aide toujours"
+	line "les dresseurs"
+	cont "qui débutent!"
+
+	para "A bientôt!"
+
+	para "..."
+	done
+
+_PayPhoneDaisyDexAreaText::
+	text "..."
+
+	para "NINA: Oui?"
+	line "Salut <PLAYER>!"
+
+	para "As-tu regardé la"
+	line "page ZONE de ton"
+	cont "#DEX? Elle"
+	cont "enregistre où"
+	cont "chaque espèce"
+	cont "de #MON vit."
+
+	para "Pratique quand"
+	line "tu cherches un"
+	cont "#MON précis!"
+
+	para "A bientôt!"
+
+	para "..."
+	done
+
+_PayPhoneDaisyGravelerEvoText::
+	text "..."
+
+	para "NINA: Oui?"
+	line "Salut <PLAYER>!"
+
+	para "As-tu remarqué un"
+	line "rocher étrange au"
+	cont "MONT SELENITE?"
+
+	para "Il y a un #MON"
+	line "ROCHE qui peut"
+	cont "évoluer rien qu'en"
+	cont "le touchant."
+
+	para "A bientôt!"
+
+	para "..."
+	done
+
+_PayPhoneDaisyRoute6AideText::
+	text "..."
+
+	para "NINA: Oui?"
+	line "Salut <PLAYER>!"
+
+	para "Pépé m’a dit que"
+	line "son ASSISTANT"
+	cont "t'attend sur la"
+	cont "ROUTE 6."
+
+	para "Tu devrais aller"
+	line "le voir!"
+
+	para "A bientôt!"
+
+	para "..."
+	done
+
+_PayPhoneDaisyMachokeEvoText::
+	text "..."
+
+	para "NINA: Oui?"
+	line "Salut <PLAYER>!"
+
+	para "J'ai entendu dire"
+	line "qu'un KARATEKA"
+	cont "s'entraîne dans la"
+	cont "GROTTE au nord de"
+	cont "LAVANVILLE."
+
+	para "Il pourrait t'aider"
+	line "à entraîner un"
+	cont "#MON COMBAT?"
+
+	para "A bientôt!"
+
+	para "..."
+	done
+
+_PayPhoneDaisyDynamicTMSellerText::
+	text "..."
+
+	para "NINA: Oui?"
+	line "Salut <PLAYER>!"
+
+	para "Savais-tu que le"
+	line "CENTRE COMMERCIAL"
+	cont "de CELADOPOLE"
+	cont "vend toutes les"
+	cont "CT existantes?"
+	cont "Incroyable, non?"
+
+	para "Mais les plus"
+	line "puissantes sont"
+	cont "réservées aux"
+	cont "dresseurs ayant"
+	cont "assez de BADGES."
+
+	para "A bientôt!"
+
+	para "..."
+	done
+
+_PayPhoneDaisyKadabraEvoText::
+	text "..."
+
+	para "NINA: Oui?"
+	line "Salut <PLAYER>!"
+
+	para "As-tu visité"
+	line "l'ACADEMIE #MON"
+	cont "à SAFRANIA? On"
+	cont "y enseigne tout"
+	cont "ce qu'il faut"
+	cont "savoir sur les"
+	cont "#MON!"
+
+	para "Des #MON PSY"
+	line "participent même"
+	cont "à leurs études."
+	cont "Tu pourrais en"
+	cont "amener un!"
+
+	para "A bientôt!"
+
+	para "..."
+	done
+
+_PayPhoneDaisyHaunterEvoText::
+	text "..."
+
+	para "NINA: Oui?"
+	line "Salut <PLAYER>!"
+
+	para "<RIVAL> m'a parlé"
+	line "d'une dame étrange"
+	cont "tout en haut de"
+	cont "la TOUR #MON"
+	cont "à LAVANVILLE."
+
+	para "D'après lui, elle"
+	line "parle aux #MON"
+	cont "SPECTRE."
+	cont "Effrayant, non?"
+
+	para "A bientôt!"
+
+	para "..."
+	done
+
+_PayPhoneDaisySaffronGuardsText::
+	text "..."
+
+	para "NINA: Oui?"
+	line "Salut <PLAYER>!"
+
+	para "Il paraît que les"
+	line "gardes aux portes"
+	cont "de SAFRANIA sont"
+	cont "parfois grognons."
+
+	para "Apporte-leur une"
+	line "boisson du CENTRE"
+	cont "COMMERCIAL, ils"
+	cont "apprécieront!"
+
+	para "A bientôt!"
+
+	para "..."
+	done
+
+_PayPhoneDaisyFishingGuideText::
+	text "..."
+
+	para "NINA: Oui?"
+	line "Salut <PLAYER>!"
+
+	para "As-tu trouvé un"
+	line "GUIDE DE PECHE?"
+
+	para "Avec ça, ton"
+	line "#DEX affichera"
+	cont "aussi les ZONEs"
+	cont "où tu peux pêcher"
+	cont "les #MON!"
+
+	para "A bientôt!"
+
+	para "..."
+	done
+
+_PayPhoneDaisyPostgameLoreleiText::
+	text "..."
+
+	para "NINA: Oui?"
+	line "Salut <PLAYER>!"
+
+	para "Il paraît qu'il y"
+	line "a des ennuis sur"
+	line "l'ILE MANDARINE."
+	cont "Des problèmes"
+	cont "avec une usine."
+
+	para "Sois prudent si tu"
+	line "y vas, d'accord?"
+
+	para "A bientôt!"
+
+	para "..."
+	done
+
+_PayPhoneDaisyPostgameBrunoText::
+	text "..."
+
+	para "NINA: Oui?"
+	line "Salut <PLAYER>!"
+
+	para "ALDO est passé aux"
+	line "infos aujourd'hui!"
+	cont "Il a été aperçu"
+	cont "à SAFRANIA."
+
+	para "ALDO est si cool!"
+	line "Tu le croiseras"
+	cont "peut-être?"
+
+	para "A bientôt!"
+
+	para "..."
+	done
+
+_PayPhoneDaisyPostgameAgathaText::
+	text "..."
+
+	para "NINA: Oui?"
+	line "Salut <PLAYER>!"
+
+	para "As-tu entendu les"
+	line "rumeurs sur la"
+	cont "TOUR #MON de"
+	cont "LAVANVILLE?"
+
+	para "Je n'ai pas compris"
+	line "tous les détails."
+	cont "Tu devrais aller"
+	cont "voir toi-même."
+
+	para "A bientôt!"
+
+	para "..."
+	done
+
+_PayPhoneDaisyPostgameLanceText::
+	text "..."
+
+	para "NINA: Oui?"
+	line "Salut <PLAYER>!"
+
+	para "On raconte qu'un"
+	line "#MON sauvage"
+	cont "sème la panique"
+	cont "à CRAMOIS'ILE,"
+	cont "près du volcan."
+
+	para "Fais attention"
+	line "si tu y vas."
+
+	para "A bientôt!"
+
+	para "..."
+	done
+
+_PayPhoneDaisyPostgameRivalText::
+	text "..."
+
+	para "<RIVAL>: C'est qui?"
+	line "Yo <PLAYER>!"
+
+	para "Tu t'attendais pas"
+	line "à tomber sur moi,"
+	cont "hein?"
+
+	para "Je suis au LABO"
+	line "avec pépé. On"
+	cont "passe en revue"
+	cont "toutes les infos"
+	cont "de mon #DEX."
+
+	para "Pas la peine de"
+	line "venir, <PLAYER>!"
+	cont "Mon #DEX est"
+	cont "bien plus rempli"
+	cont "que le tien,"
+	cont "de toute façon!"
+
+	para "Tchao, <PLAYER>!"
+
+	para "..."
+	done
+
+_PayPhoneDaisyRivalAtIndigoText::
+	text "..."
+
+	para "NINA: Oui?"
+	line "Salut <PLAYER>!"
+
+	para "<RIVAL> m'a dit"
+	line "qu'il repartait au"
+	cont "PLATEAU INDIGO."
+
+	para "Je crois qu'il"
+	line "veut une revanche"
+	cont "contre toi,"
+	cont "<PLAYER>!"
+
+	para "A bientôt!"
+
+	para "..."
+	done
+
+_PayPhoneDaisyOakRoute1Text::
+	text "..."
+
+	para "NINA: Oui?"
+	line "Salut <PLAYER>!"
+
+	para "Pépé te cherche!"
+	line "Viens au BOURG"
+	cont "PALETTE quand tu"
+	cont "auras un moment."
+
+	para "A bientôt!"
+
+	para "..."
+	done
+
 _PayPhoneNotEnoughMoneyText::
 	text "Hé! Pas d'sous,"
-	line "pas d'téléphone!"
+	line "pas d'appel!"
 	done
