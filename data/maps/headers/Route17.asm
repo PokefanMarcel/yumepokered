f396af548dfa2aef6d61390436f749dad47dbe31
Pokefan Marcel <166040321+PokefanMarcel@users.noreply.github.com>
2025-11-12 21:26:35 +0100

    Fix routes connection and spritesets

--- data/maps/headers/Route17.asm
+++ data/maps/headers/Route17.asm
@@ -1,4 +1,4 @@
 	map_header Route17, ROUTE_17, OVERWORLD, NORTH | SOUTH
-	connection north, Route16, ROUTE_16, -19 ; marcelnote - adjusted for new ROUTE_16 map size
+	connection north, Route16, ROUTE_16, -20 ; marcelnote - adjusted for new ROUTE_16 map size
 	connection south, Route18, ROUTE_18, 0
 	end_map_header
