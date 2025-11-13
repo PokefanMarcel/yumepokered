e84b1ce9c7ec4ddc04121165a4dc56bcac09407d
Pokefan Marcel <166040321+PokefanMarcel@users.noreply.github.com>
2025-11-13 10:18:11 +0100

    Translation FRA school blackboard

---
+++ translation/fra/data/text/school_blackboard.fra.asm
@@ -0,0 +1,16 @@
+; marcelnote - moved from hidden_objects/school_blackboard.asm
+HowToLinkText:
+	db   "CABLE LINK"
+	next "COLISEE"
+	next "CENTRE TROC"
+	next "NE PLUS LIRE@"
+
+StatusAilmentText1:
+	db   " SOM"
+	next " PSN"
+	next " PAR@"
+
+StatusAilmentText2:
+	db   " BRU"
+	next " GEL"
+	next " RET@"
