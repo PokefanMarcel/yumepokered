be4208b41e6524085c009ce0dd71a594d570d624
Pokefan Marcel <166040321+PokefanMarcel@users.noreply.github.com>
2026-04-25 08:11:03 -0500

    Update yes_no.asm

--- home/yes_no.asm
+++ home/yes_no.asm
@@ -2,38 +2,27 @@
 ; yes -> set carry
 YesNoChoice::
 	call SaveScreenTilesToBuffer1
-	call InitYesNoTextBoxParameters
-	jr DisplayYesNoChoice
-
-TwoOptionMenu:: ; unreferenced
-	ld a, TWO_OPTION_MENU
-	ld [wTextBoxID], a
-	call InitYesNoTextBoxParameters
-	jp DisplayTextBoxID
-
-InitYesNoTextBoxParameters::
 	xor a ; YES_NO_MENU
-	ld [wTwoOptionMenuID], a
 	hlcoord 14, 7
 	lb bc, 8, 15
-	ret
+	jr DisplayYesNoChoice
+
+;WideYesNoChoice:: ; unreferenced ; marcelnote - removed
+;	call SaveScreenTilesToBuffer1
+;	ld a, WIDE_YES_NO_MENU
+;	hlcoord 12, 7
+;	lb bc, 8, 13
+;	jr DisplayYesNoChoice
 
 YesNoChoicePokeCenter::
 	call SaveScreenTilesToBuffer1
 	ld a, HEAL_CANCEL_MENU
-	ld [wTwoOptionMenuID], a
 	hlcoord 11, 6
 	lb bc, 8, 12
-	jr DisplayYesNoChoice
-
-WideYesNoChoice:: ; unreferenced
-	call SaveScreenTilesToBuffer1
-	ld a, WIDE_YES_NO_MENU
-	ld [wTwoOptionMenuID], a
-	hlcoord 12, 7
-	lb bc, 8, 13
+	; fallthrough
 
 DisplayYesNoChoice::
+	ld [wTwoOptionMenuID], a
 	ld a, TWO_OPTION_MENU
 	ld [wTextBoxID], a
 	call DisplayTextBoxID
