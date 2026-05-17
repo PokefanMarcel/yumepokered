51f61b82bca58afaeb085f882f5d36c2c02372e1
Pokefan Marcel <166040321+PokefanMarcel@users.noreply.github.com>
2026-05-17 23:32:58 +0200

    Update Route12FishingGuideHouse.asm

--- scripts/Route12FishingGuideHouse.asm
+++ scripts/Route12FishingGuideHouse.asm
@@ -10,7 +10,7 @@ Route12FishingGuideHouseFishingGuideText: ; marcelnote - new
 	text_asm
 	CheckEvent EVENT_GOT_FISHING_GUIDE
 	ld hl, .ToAllTravelersText
-	jr nz, .print_text
+	jr nz, .printText
 	ld hl, .ItsAFishingGuideText
 	call PrintText
 	SetEvent EVENT_GOT_FISHING_GUIDE
@@ -20,7 +20,7 @@ Route12FishingGuideHouseFishingGuideText: ; marcelnote - new
 	call PlaySound
 	call WaitForSoundToFinish
 	ld hl, .AreaUpgradedText
-.print_text
+.printText
 	call PrintText
 	rst TextScriptEnd
 
