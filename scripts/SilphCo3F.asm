8ff043252df6b756ebc264a3734408fc93f790b4
Pokefan Marcel <166040321+PokefanMarcel@users.noreply.github.com>
2026-05-17 23:29:54 +0200

    Update SilphCo3F.asm

--- scripts/SilphCo3F.asm
+++ scripts/SilphCo3F.asm
@@ -44,9 +44,9 @@ SilphCo3FSilphWorkerMText:
 	text_asm
 	CheckEvent EVENT_BEAT_SILPH_CO_GIOVANNI
 	ld hl, .YouSavedUsText
-	jr nz, .print_text
+	jr nz, .printText
 	ld hl, .WhatShouldIDoText
-.print_text
+.printText
 	call PrintText
 	rst TextScriptEnd
 
