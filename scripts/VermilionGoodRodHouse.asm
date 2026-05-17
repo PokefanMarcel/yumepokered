4be224b6edfac430bc793f7f921bc1a1b2a0c7a0
Pokefan Marcel <166040321+PokefanMarcel@users.noreply.github.com>
2026-05-17 23:26:45 +0200

    Update VermilionGoodRodHouse.asm

--- scripts/VermilionGoodRodHouse.asm
+++ scripts/VermilionGoodRodHouse.asm
@@ -10,21 +10,21 @@ VermilionGoodRodHouseFishingGuruText: ; marcelnote - optimized
 	text_asm
 	CheckEvent EVENT_GOT_GOOD_ROD
 	ld hl, .HowAreTheFishBitingText
-	jr nz, .print_text
+	jr nz, .printText
 	ld hl, .DoYouLikeToFishText
 	call PrintText
 	call YesNoChoice
 	ld a, [wCurrentMenuItem]
 	and a
 	ld hl, .ThatsSoDisappointingText
-	jr nz, .print_text
+	jr nz, .printText
 	lb bc, GOOD_ROD, 1
 	call GiveItem
 	ld hl, .NoRoomText
-	jr nc, .print_text
+	jr nc, .printText
 	SetEvent EVENT_GOT_GOOD_ROD
 	ld hl, .TakeThisText
-.print_text
+.printText
 	call PrintText
 	rst TextScriptEnd ; PureRGB - rst TextScriptEnd
 
