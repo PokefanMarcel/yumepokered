2b281bbc9817566cf7296fa69a6a984e910d1087
Pokefan Marcel <166040321+PokefanMarcel@users.noreply.github.com>
2026-05-17 23:34:56 +0200

    Update PokemonTower3F.asm

--- scripts/PokemonTower3F.asm
+++ scripts/PokemonTower3F.asm
@@ -63,7 +63,7 @@ PokemonTower3FGhostBattleScript: ; marcelnote - postgame Agatha event
 	ld [wJoyIgnore], a
 	ld a, [wBattleResult]
 	and a
-	jr nz, .did_not_defeat
+	jr nz, .didNotDefeat
 	SetEvent EVENT_BEAT_GHOST_3F
 	ld a, TEXT_POKEMONTOWER3F_GHOST_VANISHED
 	ldh [hTextID], a
@@ -74,7 +74,7 @@ PokemonTower3FGhostBattleScript: ; marcelnote - postgame Agatha event
 	ld [wPokemonTower3FCurScript], a
 	ld [wCurMapScript], a
 	ret
-.did_not_defeat
+.didNotDefeat
 	ld a, $1
 	ld [wSimulatedJoypadStatesIndex], a
 	ld a, PAD_LEFT
