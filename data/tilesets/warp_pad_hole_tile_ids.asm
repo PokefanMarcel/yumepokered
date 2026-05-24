bf205e35374cd5e69ffebd4aa9a4c214742bdbe0
Pokefan Marcel <166040321+PokefanMarcel@users.noreply.github.com>
2026-05-24 23:28:23 +0200

    Update warp_pad_hole_tile_ids.asm

--- data/tilesets/warp_pad_hole_tile_ids.asm
+++ data/tilesets/warp_pad_hole_tile_ids.asm
@@ -1,6 +1,6 @@
 WarpPadAndHoleData:
 	; tileset id, tile id, value for [wStandingOnWarpPadOrHole]
-	db FACILITY, $20, 1 ; warp pad
+	db FACILITY, $22, 1 ; warp pad ; marcelnote - refactored warp engine, was $20
 	db FACILITY, $11, 2 ; hole
 	db CAVERN,   $22, 2 ; hole
 	db INTERIOR, $55, 1 ; warp pad
