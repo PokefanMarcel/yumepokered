136254342d33f66cec497f1a7be96236b37edd3d
Pokefan Marcel <166040321+PokefanMarcel@users.noreply.github.com>
2026-06-04 23:20:29 +0200

    Update RockTunnelPokecenter.esp.asm

--- translation/esp/text/RockTunnelPokecenter.esp.asm
+++ translation/esp/text/RockTunnelPokecenter.esp.asm
@@ -1,7 +1,9 @@
 _RockTunnelPokecenterGentlemanText::
+	; traducción por NunWinter
 	text "¡Los #MON"
 	line "de ciertos"
-	cont "elementos son más"
+;	cont "elementos son más" ; original
+	cont "tipos son más"     ; new: "tipos" more appropriate than "elementos"
 	cont "o menos fuertes"
 	cont "que otros!"
 	done
@@ -12,8 +14,9 @@ _RockTunnelPokecenterFisherText::
 	done
 
 _RockTunnelPokecenterBenchGuyText:: ; marcelnote - BenchGuy has a sprite
+	; traducción por NunWinter
 	text "¡Dicen que hay"
-	line "GHOST en el"
+;	line "GHOST en el"     ; original
+	line "FANTASMAS en el" ; new: "FANTASMAS" more appropriate than "GHOST"
 	cont "PUEBLO LAVANDA!"
 	done
-
