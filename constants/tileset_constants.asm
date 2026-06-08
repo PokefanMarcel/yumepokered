7d80f373e8bd7d8980e6efc6cf9085e3f30798c1
Pokefan Marcel <166040321+PokefanMarcel@users.noreply.github.com>
2026-06-08 17:14:33 +0200

    Update tileset_constants.asm

--- constants/tileset_constants.asm
+++ constants/tileset_constants.asm
@@ -4,33 +4,33 @@ DEF MAP_TILESET_SIZE EQU $60
 ; Tilesets indexes (see data/tilesets/tileset_headers.asm)
 	const_def
 	const OVERWORLD    ; 0
-	const BIG_HOUSE    ; 1 ; marcelnote - renamed REDS_HOUSE_1
-	;const MART         ; 2 ; marcelnote - removed MART tileset because redundant with POKECENTER
-	const FOREST       ; 3
-	;const REDS_HOUSE_2 ; 4
-	;const DOJO         ; 5 ; marcelnote - removed DOJO tileset because redundant with GYM
-	const SAFARI       ; 5 ; marcelnote - new tileset to free space in FOREST
-	const POKECENTER   ; 6
-	const GYM          ; 7
-	const HOUSE        ; 8
-	;const FOREST_GATE  ; 9  ; marcelnote - removed FOREST_GATE and MUSEUM tilesets
-	;const MUSEUM       ; 10 ;              because redundant with GATE
-	const UNDERGROUND  ; 11
-	const GATE         ; 12
-	const SHIP         ; 13
-	const SHIP_PORT    ; 14
-	const CEMETERY     ; 15
-	const INTERIOR     ; 16
-	const CAVERN       ; 17
-	const LOBBY        ; 18
-	const MANSION      ; 19
-	const LAB          ; 20
-	const CLUB         ; 21
-	const FACILITY     ; 22
-	const PLATEAU      ; 23
-	const ACADEMY      ; 24 ; marcelnote - new Academy tileset
-	const MOUNTAIN     ; 25 ; marcelnote - new Mountain tileset
-	const BEACH_HOUSE  ; 24 ; marcelnote - new Beach House tileset from pokeyellow
-	const PLANT        ; 24 ; marcelnote - new tileset for Power Plant
+	const BIG_HOUSE    ; 1 ; marcelnote - renamed from REDS_HOUSE_1
+;	const MART         ; X ; marcelnote - removed MART tileset because redundant with POKECENTER
+	const FOREST       ; 2
+;	const REDS_HOUSE_2 ; X
+;	const DOJO         ; X ; marcelnote - removed DOJO tileset because redundant with GYM
+	const SAFARI       ; 3 ; marcelnote - new tileset to free space in FOREST
+	const POKECENTER   ; 4
+	const GYM          ; 5
+	const HOUSE        ; 6
+;	const FOREST_GATE  ; X ; marcelnote - removed FOREST_GATE and MUSEUM tilesets
+;	const MUSEUM       ; X ;              because redundant with GATE
+	const UNDERGROUND  ; 7
+	const GATE         ; 8
+	const SHIP         ; 9
+	const SHIP_PORT    ; 10
+	const CEMETERY     ; 11
+	const INTERIOR     ; 12
+	const CAVERN       ; 13
+	const LOBBY        ; 14
+	const MANSION      ; 15
+	const LAB          ; 16
+	const CLUB         ; 17
+	const FACILITY     ; 18
+	const PLATEAU      ; 19
+	const ACADEMY      ; 20 ; marcelnote - new Academy tileset
+	const MOUNTAIN     ; 21 ; marcelnote - new Mountain tileset
+	const BEACH_HOUSE  ; 22 ; marcelnote - new Beach House tileset from pokeyellow
+	const PLANT        ; 23 ; marcelnote - new tileset for Power Plant
 	const GAME         ; 24 ; marcelnote - new tileset for Game Corner
 DEF NUM_TILESETS EQU const_value
