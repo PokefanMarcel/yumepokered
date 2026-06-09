725b7544d9efe9bf1825e5ca4cc242935a6889dd
Pokefan Marcel <166040321+PokefanMarcel@users.noreply.github.com>
2026-06-09 21:34:26 +0200

    Update predef.asm

--- home/predef.asm
+++ home/predef.asm
@@ -32,19 +32,19 @@ Predef::
 	ld [rROMB], a
 	ret
 
-GetPredefRegisters::
+GetPredefRegisters:: ; marcelnote - optimized
 ; Restore the contents of register pairs
 ; when GetPredefPointer was called.
-	ld a, [wPredefHL]
-	ld h, a
-	ld a, [wPredefHL + 1]
-	ld l, a
-	ld a, [wPredefDE]
-	ld d, a
-	ld a, [wPredefDE + 1]
-	ld e, a
-	ld a, [wPredefBC]
-	ld b, a
-	ld a, [wPredefBC + 1]
+	ld hl, wPredefBC + 1
+	ld a, [hld] ; wPredefBC + 1
 	ld c, a
+	ld a, [hld] ; wPredefBC
+	ld b, a
+	ld a, [hld] ; wPredefDE + 1
+	ld e, a
+	ld a, [hld] ; wPredefDE
+	ld d, a
+	ld a, [hld] ; wPredefHL + 1
+	ld h, [hl]  ; wPredefHL
+	ld l, a
 	ret
