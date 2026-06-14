32a6241ee2d23a9307dc545a32f6f3edd9f1f687
Pokefan Marcel <166040321+PokefanMarcel@users.noreply.github.com>
2026-06-14 11:10:43 +0200

    Update FuchsiaMoveDeleterHouse.esp.asm

--- translation/esp/text/FuchsiaMoveDeleterHouse.esp.asm
+++ translation/esp/text/FuchsiaMoveDeleterHouse.esp.asm
@@ -1,98 +1,107 @@
 ; marcelnote - new location
-; TODO - Spanish translation
 _MoveDeleterGreetingText::
-	text "Um..."
-	line "Oh yes, I'm the"
-	cont "MOVE DELETER."
+;	text "Mmm..."
+;	line "Ah, sí, soy el"
+;	cont "QUITA-MOVS." ; official translation for MOVE DELETER
+	; traducción por NunWinter
+	text "Mmm... Ah, sí."
+	line "Me conocen como"
+	cont "el BORRADÓN."
 
-	para "I can make #MON"
-	line "forget moves."
+	para "Puedo hacer que"
+	line "los #MON"
+	cont "olviden ataques."
 
-	para "Shall I make a"
-	line "#MON forget?"
+	para "¿Quieres que un"
+	line "#MON olvide"
+	cont "un ataque?"
 	done
 
-; TODO - Spanish translation
 _MoveDeleterWhichMoveText::
-	text "Which move should"
-	line "it forget, then?"
+	; traducción por NunWinter
+	text "¿Qué ataque"
+	line "debe olvidar?"
 	done
 
-; TODO - Spanish translation
 _MoveDeleterConfirmText::
-	text "Oh, make it forget"
-	line "@"
+	; traducción por NunWinter
+	text "Oh, ¿quieres que"
+	line "olvide @"
 	text_ram wStringBuffer
 	text "?"
 	done
 
-; TODO - Spanish translation
 _MoveDeleterForgotText::
-	text "@"
+	; traducción por NunWinter
+	text "¡"
 	text_ram wStringBuffer
-	text " was"
-	line "forgotten!"
+	text " fue"
+	line "olvidado!"
 	done
 
-; TODO - Spanish translation
 _MoveDeleterByeText::
-	text "No? Come visit me"
-	line "again."
+	; traducción por NunWinter
+	text "¿No? Ven a verme"
+	line "otra vez."
 	done
 
-; TODO - Spanish translation
 _MoveDeleterOneMoveText::
-	text "That #MON"
-	line "knows only one"
-	cont "move."
+	; traducción por NunWinter
+	text "Este #MON"
+	line "solo conoce"
+	cont "un ataque."
 	prompt
 
-
-; TODO - Spanish translation
 _MoveReminderGreetingText::
-	text "Hello dear. People"
-	line "call me the"
-	cont "MOVE REMINDER."
+;	text "Hola, cariño."
+;	line "Me llaman la"
+;	cont "RICUERDA-MOVS." ; official translation for MOVE REMINDER
+	; traducción por NunWinter
+	text "Hola, cielo."
+	line "Me conocen como"
+	cont "la RECORDADOÑA."
 
-	para "I teach #MON to"
-	line "remember moves"
-	cont "they forgot."
+	para "Ayudo a los"
+	line "#MON a"
+	cont "recordar ataques"
+	cont "olvidados."
 
-	para "¥1000 per lesson."
-	line "How about it?"
+	para "1000¥ por lección."
+	line "¿Qué te parece?"
 	done
 
-; TODO - Spanish translation
 _MoveReminderSaidYesText::
-	text "Which #MON"
-	line "should I teach?"
+	; traducción por NunWinter
+	text "¿Qué #MON va a"
+	line "recordar?"
 	prompt
 
-; TODO - Spanish translation
 _MoveReminderNotEnoughMoneyText::
-	text "Hmmm..."
+	; traducción por NunWinter
+	text "Mmm..."
 
-	para "You don't have"
-	line "enough money!"
+	para "¡No tienes"
+	line "suficiente dinero!"
 	done
 
-; TODO - Spanish translation
 _MoveReminderWhichMoveText::
-	text "Which move should"
-	line "it learn?"
+	; traducción por NunWinter
+	text "¿Qué ataque"
+	line "debe aprender?"
 	done
 
-; TODO - Spanish translation
 _MoveReminderByeText::
-	text "If any of your"
-	line "#MON needs to"
-	cont "remember a move,"
-	cont "come visit me!"
+	; traducción por NunWinter
+	text "Si alguno de"
+	line "tus #MON"
+	cont "necesita"
+	cont "recordar ataques,"
+	cont "¡ven a verme!"
 	done
 
-; TODO - Spanish translation
 _MoveReminderNoMovesText::
-	text "This #MON"
-	line "hasn't forgotten"
-	cont "any moves."
+	; traducción por NunWinter
+	text "Este #MON no ha"
+	line "olvidado ningún"
+	cont "ataque."
 	prompt
