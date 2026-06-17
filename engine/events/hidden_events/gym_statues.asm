a4f111299f0b8ce588e46064bf5accf85c2d2499
Pokefan Marcel <166040321+PokefanMarcel@users.noreply.github.com>
2026-06-17 23:35:18 +0200

    Update gym_statues.asm

--- engine/events/hidden_events/gym_statues.asm
+++ engine/events/hidden_events/gym_statues.asm
@@ -1,6 +1,7 @@
 GymStatues:
 ; if in a gym and have the corresponding badge, a = GymStatueText2_id and jp PrintPredefTextID
 ; if in a gym and don't have the corresponding badge, a = GymStatueText1_id and jp PrintPredefTextID
+; if in a gym and have the corresponding badge and the next one, a = GymStatueText3_id and jp PrintPredefTextID
 ; else ret
 	ld a, [wSpritePlayerStateData1FacingDirection]
 	cp SPRITE_FACING_UP
@@ -17,24 +18,19 @@ GymStatues:
 	jr z, .match
 	inc hl
 	jr .loop
-.match
-	ld b, [hl]
+.match ; marcelnote - modified this part to add Yellow
+	ld b, [hl]              ; b = 1 << bit current badge
 	ld a, [wObtainedBadges] ; marcelnote - removed redundant wBeatGymFlags
-	and b
-	cp b
-;;;;;; marcelnote - modified this part to add Yellow
+	and b                   ; do we have the badge of the current gym?
 	tx_pre_id GymStatueText1
-	jr nz, .print
-	CheckEvent EVENT_BECAME_CHAMPION ; if became champion...
+	jr z, .print            ; if not, just mention Rival's name
+	ld a, [wObtainedBadges]
+	sla b                   ; b = 1 << bit next badge
+	and b                   ; do we also have the badge of the next gym?
 	tx_pre_id GymStatueText2
-	jr z, .print
-	ld a, [wCurMap]
-	cp VIRIDIAN_GYM                  ; and not in Viridian Gym...
-	tx_pre_id GymStatueText2
-	jr z, .print
-	tx_pre_id GymStatueText3         ; then also print YELLOW
+	jr z, .print            ; if not, also mention Player's name
+	tx_pre_id GymStatueText3 ; if yes, even mention Yellow's name
 .print
-;;;;;;
 	jp PrintPredefTextID
 
 INCLUDE "data/maps/badge_maps.asm"
