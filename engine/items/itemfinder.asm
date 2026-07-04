94ed9bf931cceaef925bde65c0b383a40e2f54a8
Pokefan Marcel <166040321+PokefanMarcel@users.noreply.github.com>
2026-07-04 11:49:09 +0200

    HiddenItemNear optim

--- engine/items/itemfinder.asm
+++ engine/items/itemfinder.asm
@@ -1,11 +1,11 @@
-HiddenItemNear:
+HiddenItemNear: ; marcelnote - optimized
 	ld hl, HiddenItemCoords
 	ld b, 0
 .loop
 	ld de, 3
 	ld a, [wCurMap]
 	call IsInRestOfArray
-	ret nc ; return if current map has no hidden items
+	ret nc ; return if current map has no more hidden items
 	push bc
 	push hl
 	ld hl, wObtainedHiddenItemsFlags
@@ -16,37 +16,27 @@ HiddenItemNear:
 	pop hl
 	pop bc
 	inc b
-	and a
+	and a ; has the item already be obtained?
 	inc hl
-	ld d, [hl]
-	inc hl
-	ld e, [hl]
-	inc hl
+	ld a, [hli]
+	ld d, a ; item's Y coord
+	ld a, [hli]
+	ld e, a ; item's X coord
 	jr nz, .loop ; if the item has already been obtained
 ; check if the item is within 4-5 tiles (depending on the direction of item)
+; marcelnote - optimized and fixed coordinates X=0 and Y=0 being ignored
+	; set carry flag if:  0 <= playerY - itemY + 4 <= 8,
+	;               i.e.  playerY - 4 <= itemY <= playerY + 4
 	ld a, [wYCoord]
-	call Sub5ClampTo0
-	cp d
-	jr nc, .loop
-	ld a, [wYCoord]
+	sub d
 	add 4
-	cp d
-	jr c, .loop
-	ld a, [wXCoord]
-	call Sub5ClampTo0
-	cp e
+	cp 9
 	jr nc, .loop
+	; set carry flag if:  0 <= playerX - itemX + 5 <= 9,
+	;               i.e.  playerX - 4 <= itemX <= playerX + 5
 	ld a, [wXCoord]
+	sub e
 	add 5
-	cp e
-	jr c, .loop
-	scf
-	ret
-
-Sub5ClampTo0:
-; subtract 5 but clamp to 0
-	sub 5
-	cp $f0
-	ret c
-	xor a
-	ret
+	cp 10
+	jr nc, .loop
+	ret ; carry flag set
