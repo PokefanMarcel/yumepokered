a3fac6b3d3631625c9dfcb478dbb9635a1b09dcc
Pokefan Marcel <166040321+PokefanMarcel@users.noreply.github.com>
2026-07-22 00:08:21 +0200

    Update CeruleanBadgeHouse.asm

--- scripts/CeruleanBadgeHouse.asm
+++ scripts/CeruleanBadgeHouse.asm
@@ -16,11 +16,10 @@ CeruleanBadgeHouseMiddleAgedManText:
 	call PrintText
 	ld hl, .BadgeItemList
 	call LoadItemList
-	ld hl, wItemList
-	ld a, l
-	ld [wListPointer], a
-	ld a, h
-	ld [wListPointer + 1], a
+	ld hl, wListPointer ; marcelnote - optimized load in wListPointer
+	ld a, LOW(wItemList)
+	ld [hli], a
+	ld [hl], HIGH(wItemList)
 	xor a
 	ld [wPrintItemPrices], a
 	ld [wMenuItemToSwap], a
